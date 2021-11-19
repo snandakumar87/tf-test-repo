80d301e05207d797287f3e313c3fa5914df44dc9
Sadhana Nandakumar <39506194+snandakumar87@users.noreply.github.com>
2021-11-19 15:04:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
 #main
+provider "vsphere" {
+  vsphere_server       = "${var.vsphere_server}"
+  user                 = "${var.vsphere_user}"
+  password             = "${var.vsphere_password}"
+  allow_unverified_ssl = true
+}
+
+variable "vsphere_user" {}
+variable "vsphere_password" {}
+variable "vsphere_server" {}
