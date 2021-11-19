54f963f77274345b445d909da7b6b9b1d229517d
Sadhana Nandakumar <39506194+snandakumar87@users.noreply.github.com>
2021-11-19 15:04:56 -0500

    Update vm01.tf

--- vm01.tf
+++ vm01.tf
@@ -1,1 +1,20 @@
 #VM01
+module "vm01" {
+  source  = "./terraform-vsphere-datacenter"
+
+  vm_name           = "tf-vm02"
+  vm_size           = "small"
+  vm_folder         = "VMaaS"
+  vm_template       = "RHEL84"
+  region            = "canada"
+
+  data_disk = {
+    disk1 = {
+      size_gb = 10,
+      thin_provisioned = false
+    }
+    disk2 = {
+      size_gb = 10
+    }
+  }
+}
